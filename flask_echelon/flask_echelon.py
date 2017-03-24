f692e3c498e428d26f792709ca5942339666a5da
Jesse Roberts <jesse@jesseops.net>
2017-03-24 00:05:43 -0600

    support defining echelons

--- flask_echelon/flask_echelon.py
+++ flask_echelon/flask_echelon.py
@@ -16,13 +16,42 @@ class EchelonManager:
         self._separator = separator
         self._mongo_collection = collection
         if app:
+            self.app = app
             self.init_app(app)
 
     def init_app(self, app):
+        self.db[self._mongo_collection].create_index("echelon", unique=True)
         app.echelon_manager = self
+
+    def define_echelon(self, echelon, name=None, help=None):
+        """
+        Creates or updates an Echelon definition
+
+        :param echelon: (str) Representation of a single Echelon within
+        a permission hierarchy
+        :param name: (str) Pretty name for a given Echelon
+        :param help: (str) Help text defining Echelon purpose/scope
+        :return: None
+        """
+        if echelon.startswith(self._separator):
+            raise Exception('{} leads with separator "{}"'.format(echelon, self._separator))
+
+        init = {'groups': [], 'users': []}
+
+        payload = {"echelon": echelon,
+                   "name": name or echelon,
+                   "help": help or "Provides access to {}".format(echelon)}
+
+        self.db[self._mongo_collection].update({"echelon": echelon},
+                                               {"$set": payload, "$setOnInsert": init},
+                                               upsert=True)
 
     @property
     def db(self):
-        if not self._db:
-            return current_app.db
-        return self._db
+        if self._db is not None:
+            return self._db
+        if self.app:
+            try:
+                return self.app.db
+            except AttributeError:
+                raise Exception('No database defined on manager or current_app')
--- tests/test_flask_echelon.py
+++ tests/test_flask_echelon.py
@@ -12,10 +12,32 @@ import pytest
 
 
 from flask import Flask
+from pymongo import MongoClient
 from flask_echelon import EchelonManager
+
+
+# only use one MongoClient instance
+DB = MongoClient().test_flask_echelon
+
+
+def setup_function(function):
+    DB.echelons.drop()
+
+
+def teardown_function(function):
+    DB.echelons.drop()
 
 
 def test_000_init():
     """Can be initialized as a Flask plugin or standalone"""
     EchelonManager()
-    EchelonManager(app=Flask(__name__))
+    with pytest.raises(Exception):
+        EchelonManager(app=Flask(__name__))
+    EchelonManager(app=Flask(__name__), database=DB)
+
+
+def test_001_define():
+    """Can define interactions successfully"""
+    manager = EchelonManager(database=DB)
+    echelon = "foo::bar::baz"
+    manager.define_echelon(echelon, name="I test things", help="It's a test, ok?")
